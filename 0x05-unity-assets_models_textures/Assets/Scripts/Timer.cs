be5ddc1af654896f695369c814664cede922cb16
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-06-08 14:22:11 -0500

    [Feature]: WinTrigger done !!

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+/// <summary> Class Timer, handles time </summary>
+public class Timer : MonoBehaviour
+{
+    /// <summary> UI Text </summary>
+    public Text timeText;
+    /// <summary> Time in float </summary>
+    public float timer;
+    int MinuteCount;
+    int SecondCount;
+    float MilliCount;
+
+    void Start()
+    {
+        timer = 0.0f;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        timer += 1 * Time.deltaTime;
+        MinuteCount = (int)timer/ 60;
+        SecondCount = (int)timer% 60;
+        MilliCount += Time.deltaTime * 100;
+        if (MilliCount >= 100)
+        {
+            MilliCount = 0;
+        }
+    
+        timeText.text = string.Format("{0:0}:{1:00}.{2:00}", MinuteCount, SecondCount, MilliCount);   
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary> Class in charge of triggering Timer script </summary>
+public class TimerTrigger : MonoBehaviour
+{
+    /// <summary> GameObject where Timer script is attached </summary>
+    public GameObject player;
+    void OnTriggerExit(Collider other)
+    {
+        player.GetComponent<Timer>().enabled = true;
+    }
+}
