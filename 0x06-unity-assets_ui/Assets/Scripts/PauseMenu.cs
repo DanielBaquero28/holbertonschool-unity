b61ec8f136a73545e5243b47060f09e1bbf8e0bf
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-06-23 21:07:40 -0500

    [Feature]: PauseMenu done !

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    /// <summary> Pause Canvas GameObject </summary>
+    public GameObject PauseCanvas;
+
+    private bool pausedMenu = false;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Debug.Log("Entered Update");
+            if (pausedMenu == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }  
+
+    public void Pause()
+    {
+        pausedMenu = true;
+        PauseCanvas.SetActive(true);
+        Cursor.lockState = CursorLockMode.Confined;
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pausedMenu = false;
+        PauseCanvas.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
