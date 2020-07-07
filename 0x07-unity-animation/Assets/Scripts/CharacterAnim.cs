35a79e7b79e85fdc0305e09878e2e2f9c0d62b84
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-07-07 17:58:01 -0500

    [Feature]: Running animation

---
+++ 0x07-unity-animation/Assets/Scripts/CharacterAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+}
