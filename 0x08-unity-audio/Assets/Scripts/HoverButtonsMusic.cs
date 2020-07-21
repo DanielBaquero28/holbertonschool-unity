f5203a7b1fa1146fff030eda0e3520101d04aec7
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-07-21 17:15:26 -0500

    [Feature]: Hover buttons sound done!

---
+++ 0x08-unity-audio/Assets/Scripts/HoverButtonsMusic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class HoverButtonsMusic : MonoBehaviour
+{
+    AudioSource audioS;
+    
+    void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+    }
+    void PlaySound()
+    {
+        audioS.Play();
+    }
+}
