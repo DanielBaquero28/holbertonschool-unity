ba62913d9558c7213e2deb1c4718031c8b67b331
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-11-06 17:53:49 -0500

    [Feature]: Building to test touch features and plane detection

---
+++ 0x0C-unity-ar_slingshot_game/Assets/Scripts/PlaneManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using Unity.Collections;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaneManager : MonoBehaviour
+{
+    ARRaycastManager _RaycastManager;
+    ARPlaneManager _PlaneManager;
+    
+    static ARPlane chosenPlane;
+    static List<ARRaycastHit> raycastHits = new List<ARRaycastHit>();
+
+    public GameObject startButton;
+
+    int flag = 0;
+    void Awake()
+    {
+        _RaycastManager = GetComponent<ARRaycastManager>();
+        _PlaneManager = GetComponent<ARPlaneManager>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.touchCount > 0 && flag == 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended && Input.touchCount == 1)
+            {
+                if(_RaycastManager.Raycast(touch.position, raycastHits, TrackableType.PlaneWithinPolygon))
+                {
+                    chosenPlane = _PlaneManager.GetPlane(raycastHits[0].trackableId);
+
+                    foreach (var plane in _PlaneManager.trackables)
+                    {
+                        plane.gameObject.SetActive(false);
+                    }
+                    startButton.SetActive(true);
+                    flag = 1;
+                }
+            }
+        }
+    }
+}
+    
